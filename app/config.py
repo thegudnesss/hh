30ae0ac8a086ab1f174025540163074dc11cec93
thegudnesss <thegudnesss@gmail.com>
2025-11-29 22:56:04 +0500

    commit 2

--- app/config.py
+++ app/config.py
@@ -1,2 +1,22 @@
-from bot.src.config import config
+from typing import Optional
 
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class ClickSettings(BaseSettings):
+    # Click Payment Settings
+    CLICK_SERVICE_ID: Optional[str] = None
+    CLICK_MERCHANT_ID: Optional[str] = None
+    CLICK_MERCHANT_USER_ID: Optional[str] = None
+    CLICK_SECRET_KEY: Optional[SecretStr] = None
+    CLICK_TEST_MODE: bool = False
+
+    model_config = SettingsConfigDict(
+        env_file = "app/data/.env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+    )
+
+
+config = ClickSettings()
--- bot/src/config.py
+++ bot/src/config.py
@@ -9,15 +9,8 @@ class Settings(BaseSettings):
     MONGO_URL: SecretStr
     DB_CHANNEL: Optional[str] = None
 
-    # Click Payment Settings
-    CLICK_SERVICE_ID: Optional[str] = None
-    CLICK_MERCHANT_ID: Optional[str] = None
-    CLICK_MERCHANT_USER_ID: Optional[str] = None
-    CLICK_SECRET_KEY: Optional[SecretStr] = None
-    CLICK_TEST_MODE: bool = False
-
     model_config = SettingsConfigDict(
-        env_file = "data/.env",
+        env_file = "bot/data/.env",
         env_file_encoding="utf-8",
         case_sensitive=False,
     )
